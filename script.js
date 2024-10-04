8eb9ab1a9d8c73437bd003412b3ea8a1b26838bc
Anshuman Gupta <132660695+anshuman2604@users.noreply.github.com>
2024-10-04 23:16:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const card = document.querySelector('.profile-card');
+    setTimeout(() => {
+        card.style.opacity = '1';
+    }, 500);
+});
